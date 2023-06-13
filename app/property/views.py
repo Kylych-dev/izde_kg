c180fa37e631663a26165af47ad34615d484da9c
Erkin <erkin.tfr@gmail.com>
2023-06-13 17:46:49 +0600

    paginator

--- app/property/views.py
+++ app/property/views.py
@@ -6,6 +6,7 @@ from .models import Property
 from .serializers import PropertySerializer
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.pagination import PageNumberPagination
 
 
 class PropertyReadView(viewsets.ReadOnlyModelViewSet):
@@ -15,16 +16,27 @@ class PropertyReadView(viewsets.ReadOnlyModelViewSet):
     queryset = Property.objects.all()
     serializer_class = PropertySerializer
     
+class CustomPagination(PageNumberPagination):
+    page_size = 10 
+    page_size_query_param = 'page_size' 
+    max_page_size = 100  
 
 class PropertyView(viewsets.ModelViewSet):
     """
     Класс для добавления, редактирования и удаления объявления, работает только для владельца
     """
-    queryset = Property.objects.all()  #filter(owner = request.user)
+    pagination_class = CustomPagination
     serializer_class = PropertySerializer
     permission_classes = [IsAuthenticated, IsAdminUser]
     
-    @action(detail=False, methods=['get'])
+    def get(self, request):
+        queryset = Property.objects.all()  #filter(owner = request.user)
+        paginated_data = self.pagination_class().paginate_queryset(queryset, request)
+
+        return self.pagination_class().get_paginated_response(paginated_data)
+
+
+    # @action(detail=False, methods=['get'])
     def search(self, request, **kwargs):
         property = self.get_queryset()
         kwarg = {}
