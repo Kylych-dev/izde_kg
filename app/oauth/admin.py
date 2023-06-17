fe231eab15f821ce532579e0532696350afb771f
Mirbekov <@kylych-bob>
2023-06-17 14:26:20 +0600

    adding answer yes or no choice field

--- app/oauth/admin.py
+++ app/oauth/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from . import models
 from app.realtor.models import Agent
+from app.realtor.models import Language
 
 
 @admin.register(models.CustomUser)
@@ -12,4 +13,13 @@ class UserModelAdmin(admin.ModelAdmin):
     #     model = models.UserModel
 
 
+@admin.register(models.Language)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    list_filter = ['title']
+
+
+
+
 admin.site.register(Agent)
+admin.site.register(Language)
